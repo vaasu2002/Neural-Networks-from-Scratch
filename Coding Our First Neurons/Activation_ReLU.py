5fbdca2a7a9f4e01d7f9e8e993dc0bfcc2402ab4
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-04-17 00:35:40 +0530

    Delete Coding Our First Neurons directory

--- Coding Our First Neurons/Activation_ReLU.py
+++
@@ -1,3 +0,0 @@
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
--- Coding Our First Neurons/Activation_Softmax.py
+++
@@ -1,5 +0,0 @@
-class Activation_Softmax:
-    def forward(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        self.output = 
